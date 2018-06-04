b8d9ff1458bcde3e5ca7ff3472386ec83132f543
David Völgyes <david.volgyes@ieee.org>
2018-06-04 11:04:35 +0200

    updated tests, improved code coverage

--- tests/test.sh
+++ tests/test.sh
@@ -6,14 +6,17 @@ PYTHON="python3 -m coverage run -a --source $DIR/../src/"
 $PYTHON $DIR/../src/zenodo_get.py
 $PYTHON $DIR/../src/zenodo_get.py -h
 
-# expected failed tests
+# tests expected to fail
 $PYTHON $DIR/../src/zenodo_get.py invalid_doi && false || true
 $PYTHON $DIR/../src/zenodo_get.py -1 x && false || true
+$PYTHON $DIR/../src/zenodo_get.py 0 x && false || true
 $PYTHON $DIR/../src/zenodo_get.py https://invalid && false || true
 
-$PYTHON $DIR/../src/zenodo_get.py 1215979
-$PYTHON $DIR/../src/zenodo_get.py -r 1215979
-$PYTHON $DIR/../src/zenodo_get.py 10.5281/zenodo.1215979
+# tests expected to pass
+$PYTHON $DIR/../src/zenodo_get.py 1215979 -m -e -k
+$PYTHON $DIR/../src/zenodo_get.py -r 1215979 -w urls.txt -n
+$PYTHON $DIR/../src/zenodo_get.py -r 1215979 -w -
+$PYTHON $DIR/../src/zenodo_get.py 10.5281/zenodo.1215979 -R 3 -p 2 -n
 $PYTHON $DIR/../src/zenodo_get.py -d 10.5281/zenodo.1215979
 
 echo "  TESTS ARE OK!  "
--- tests/win-test.bat
+++ tests/win-test.bat
@@ -1,8 +1,8 @@
 REM #
-
 cd tests
-python ../src/zenodo_get.py 10.5281/zenodo.1215979
-python ../src/zenodo_get.py -d 10.5281/zenodo.1215979
-python ../src/zenodo_get.py -r 1215979
+python ../src/zenodo_get.py 10.5281/zenodo.1215979 -m -e -k
+python ../src/zenodo_get.py -d 10.5281/zenodo.1215979 -w urls.txt -n
+python ../src/zenodo_get.py -r 1215979 -w -
+python ../src/zenodo_get.py 1215979 -R 3 -p 2 -n
 python ../src/zenodo_get.py 1215979
 
