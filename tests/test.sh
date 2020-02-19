8c1d32e758cc88443ec22e98c3963ad3d6495ed3
David Völgyes <david.volgyes@ieee.org>
2020-02-19 16:47:30 +0100

    fixed code coverage, hopefully

--- tests/test.sh
+++ tests/test.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -e
-DIR=$(dirname "$0")
+cd ..
 
-
-PYTHON="python3 -m coverage run -a --source $DIR/../src/"
-$PYTHON $DIR/../src/zenodo_get.py
-$PYTHON $DIR/../src/zenodo_get.py -h
+PYTHON="python3 -m coverage run -a --source src/"
+$PYTHON ./zenodo_get.py
+$PYTHON ./zenodo_get.py -h
 
 # tests expected to fail
-$PYTHON $DIR/../src/zenodo_get.py invalid_doi && false || true
-$PYTHON $DIR/../src/zenodo_get.py -1 x && false || true
-$PYTHON $DIR/../src/zenodo_get.py 0 x && false || true
-$PYTHON $DIR/../src/zenodo_get.py https://invalid && false || true
+$PYTHON ./zenodo_get.py invalid_doi && false || true
+$PYTHON ./zenodo_get.py -1 x && false || true
+$PYTHON ./zenodo_get.py 0 x && false || true
+$PYTHON ./zenodo_get.py https://invalid && false || true
 
 # tests expected to pass
-$PYTHON $DIR/../src/zenodo_get.py 1215979 -m -e -k
-$PYTHON $DIR/../src/zenodo_get.py -r 1215979 -w urls.txt -n
-$PYTHON $DIR/../src/zenodo_get.py -r 1215979 -w -
-$PYTHON $DIR/../src/zenodo_get.py 10.5281/zenodo.1215979 -R 3 -p 2 -n
-$PYTHON $DIR/../src/zenodo_get.py -d 10.5281/zenodo.1215979
+$PYTHON ./zenodo_get.py 1215979 -m -e -k
+$PYTHON ./zenodo_get.py -r 1215979 -w urls.txt -n
+$PYTHON ./zenodo_get.py -r 1215979 -w -
+$PYTHON ./zenodo_get.py 10.5281/zenodo.1215979 -R 3 -p 2 -n
+$PYTHON ./zenodo_get.py -d 10.5281/zenodo.1215979
 
 echo "  TESTS ARE OK!  "
