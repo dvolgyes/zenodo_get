6de57e5cc26b0ce79e872d3a300ff15ff6e88576
David Völgyes <david.volgyes@ieee.org>
2020-02-20 12:00:00 +0100

    test fixes

--- tests/test.sh
+++ tests/test.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
 
-PYTHON="python3 -m coverage run -a --source zenodo_get"
-$PYTHON zenodo_get
-$PYTHON zenodo_get -h
-$PYTHON zenodo_get --cite
+CMD="python3 -m coverage run -a --source zenodo_get -m zenodo_get "
+$CMD 
+$CMD  -h
+$CMD  --cite
 
 # tests expected to fail
-$PYTHON zenodo_get invalid_doi && false || true
-$PYTHON zenodo_get -1 x && false || true
-$PYTHON zenodo_get 0 x && false || true
-$PYTHON zenodo_get https://invalid && false || true
+$CMD  invalid_doi && false || true
+$CMD  -1 x && false || true
+$CMD  0 x && false || true
+$CMD  https://invalid && false || true
 
 # tests expected to pass
-$PYTHON zenodo_get 1215979 -m -e -k
-$PYTHON zenodo_get -r 1215979 -w urls.txt -n
-$PYTHON zenodo_get -r 1215979 -w -
-$PYTHON zenodo_get 10.5281/zenodo.1215979 -R 3 -p 2 -n
-$PYTHON zenodo_get -d 10.5281/zenodo.1215979
+$CMD  1215979 -m -e -k
+$CMD  -r 1215979 -w urls.txt -n
+$CMD  -r 1215979 -w -
+$CMD  10.5281/zenodo.1215979 -R 3 -p 2 -n
+$CMD  -d 10.5281/zenodo.1215979
 
 echo "  TESTS ARE OK!  "
