845b6959ddd457e7544865481320ddaf909b6f5d
David Völgyes <volgyes@stcorp.no>
2021-11-20 00:34:41 +0100

    [fix] add python 3.9/3.10 support

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU Affero General Public License v3',
     ],
