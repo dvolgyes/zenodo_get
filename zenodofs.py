bf544f559ddac0915c95936eba4f72b8ecd05b75
Casper da Costa-Luis <casper.dcl@physics.org>
2024-04-10 16:15:30 +0100

    lint: bump & run pre-commit hooks

--- zenodofs.py
+++ zenodofs.py
@@ -14,7 +14,7 @@ try:
     from fuse import FUSE, Operations, LoggingMixIn
 except ImportError as e:
     logging.getLogger().critical(e)
-    logging.getLogger().critical('You need to install python-box, requests and fusepy.')
+    logging.getLogger().critical("You need to install python-box, requests and fusepy.")
     sys.exit(1)
 
 
@@ -84,16 +84,17 @@ class WebFile:
             N = max(end - start, 0)
 
             if domain.start + N < domain.stop:
-                response = response + self[domain.start + N:domain.stop]
+                response = response + self[domain.start + N : domain.stop]
         return response
 
 
 class ZenodoFS(LoggingMixIn, Operations):
 
     def __init__(self, recordIDs, sandbox_recordIDs, chunksize=64, largefile=1024):
-        self.records = {'sandbox': [],
-                        'zenodo': [],
-                        }
+        self.records = {
+            "sandbox": [],
+            "zenodo": [],
+        }
         self.attr_cache = SBox(default_box=True)
         self.dir_cache = SBox(default_box=True)
         self.open_files = {}
@@ -109,95 +110,109 @@ class ZenodoFS(LoggingMixIn, Operations):
     @cache(maxsize=1024)
     def get_metadata(self, recordID, sandbox=False, exceptions=True, timeout=15):
         if not sandbox:
-            url = 'https://zenodo.org/api/records/'
+            url = "https://zenodo.org/api/records/"
         else:
-            url = 'https://sandbox.zenodo.org/api/records/'
+            url = "https://sandbox.zenodo.org/api/records/"
 
         try:
             r = requests.get(url + recordID, timeout=timeout)
         except requests.exceptions.ConnectTimeout:
-            self.logger.critical('Connection timeout during metadata reading.')
+            self.logger.critical("Connection timeout during metadata reading.")
             raise
         except Exception:
-            self.logger.critical('Connection error during metadata reading.')
+            self.logger.critical("Connection error during metadata reading.")
             raise
 
         js = {}
         if r.ok:
-            js = json.loads(r.text)['files']
-            for f in json.loads(r.text)['files']:
-                path = 'zenodo' if not sandbox else 'sandbox'
+            js = json.loads(r.text)["files"]
+            for f in json.loads(r.text)["files"]:
+                path = "zenodo" if not sandbox else "sandbox"
                 self.attr_cache[f'/{path}/{recordID}/{f["key"]}'] = SBox(
-                    f, default_box=True)
+                    f, default_box=True
+                )
 
-            self.content[f"/{path}/{recordID}.json"] = (SBox(metadata=js).to_json() + "\n").encode()
-            self.content[f"/{path}/{recordID}.yaml"] = SBox(metadata=js).to_yaml().encode()
+            self.content[f"/{path}/{recordID}.json"] = (
+                SBox(metadata=js).to_json() + "\n"
+            ).encode()
+            self.content[f"/{path}/{recordID}.yaml"] = (
+                SBox(metadata=js).to_yaml().encode()
+            )
         return js
 
     def readdir(self, path, fh):
-        level = len(path.split('/'))
-        content = [name for name in self.attr_cache.keys()
-                   if name.startswith(path)]
-        if path == '/':
-            return ['.', 'sandbox', 'zenodo']
+        level = len(path.split("/"))
+        content = [name for name in self.attr_cache.keys() if name.startswith(path)]
+        if path == "/":
+            return [".", "sandbox", "zenodo"]
 
-        elif path in ('/sandbox', '/zenodo'):
-            content = [name for name in self.attr_cache.keys()
-                       if name.startswith(path)]
+        elif path in ("/sandbox", "/zenodo"):
+            content = [name for name in self.attr_cache.keys() if name.startswith(path)]
         else:
-            parts = path.split('/')
+            parts = path.split("/")
             if len(parts) >= 3:
                 recordID = parts[2]
                 self.get_metadata(recordID)
-                content = [name for name in self.attr_cache.keys()
-                           if name.startswith(path)]
+                content = [
+                    name for name in self.attr_cache.keys() if name.startswith(path)
+                ]
 
         N = len(path) + 1
-        content = list({name[N:].split('/')[0] for name in content if len(name) > N and name[N-1] == '/'})
+        content = list(
+            {
+                name[N:].split("/")[0]
+                for name in content
+                if len(name) > N and name[N - 1] == "/"
+            }
+        )
         if level == 2:
-            content = content + [f"{name}.yaml" for name in content if name.find('.') == -1] + [f"{name}.json" for name in content if name.find('.') == -1]
+            content = (
+                content
+                + [f"{name}.yaml" for name in content if name.find(".") == -1]
+                + [f"{name}.json" for name in content if name.find(".") == -1]
+            )
         return list(set(content))
 
     def getattr(self, path, fh=None):
-        parts = path.split('/')
+        parts = path.split("/")
         level = len(parts)
         st = {}
-        if path in ['/', '/sandbox', '/zenodo']:
-            st['st_mode'] = (S_IFDIR | 0o755)
-            st['st_nlink'] = 2
+        if path in ["/", "/sandbox", "/zenodo"]:
+            st["st_mode"] = S_IFDIR | 0o755
+            st["st_nlink"] = 2
         elif level == 3:
-            if path.find('.') > -1:
+            if path.find(".") > -1:
                 size = len(self.content[path])
-                st = {'st_mode': (S_IFREG | 0o444), 'st_size': size}
+                st = {"st_mode": (S_IFREG | 0o444), "st_size": size}
             else:
-                st['st_mode'] = (S_IFDIR | 0o755)
-                st['st_nlink'] = 2
+                st["st_mode"] = S_IFDIR | 0o755
+                st["st_nlink"] = 2
         else:
             size = 0
-            st = {'st_mode': (S_IFREG | 0o444), 'st_size': size}
+            st = {"st_mode": (S_IFREG | 0o444), "st_size": size}
             if level >= 3:
                 recordID = parts[2]
                 self.get_metadata(recordID)
 
             if level == 4:
                 fn = self.attr_cache[path]
-                if 'size' in fn:
-                    st['st_size'] = fn['size']
-        st['st_ctime'] = st['st_mtime'] = st['st_atime'] = time()
+                if "size" in fn:
+                    st["st_size"] = fn["size"]
+        st["st_ctime"] = st["st_mtime"] = st["st_atime"] = time()
         return st
 
     def open(self, path, mode):
         if path not in self.open_files:
-            url = self.attr_cache[path]['links'].get('self')
-            size = self.attr_cache[path].get('size', 0)
+            url = self.attr_cache[path]["links"].get("self")
+            size = self.attr_cache[path].get("size", 0)
             self.open_files[path] = WebFile(url, size, self.chunksize, self.largefile)
         return 0
 
     def read(self, path, size, offset, fh):
         if path in self.content:
-            return self.content[path][offset:offset + size]
+            return self.content[path][offset : offset + size]
 
-        return self.open_files[path][offset:offset + size]
+        return self.open_files[path][offset : offset + size]
 
     def release(self, path, fh):
         if path in self.open_files:
@@ -205,44 +220,72 @@ class ZenodoFS(LoggingMixIn, Operations):
             wf.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("mountpoint", type=str, help="mount point")
-    parser.add_argument("-r", "--record", nargs='+', action='extend', default=[],
-                        help='record ID(s)')
-    parser.add_argument("-s", "--sandbox", nargs='+', action='extend', default=[],
-                        help='sandbox record ID(s)')
-    parser.add_argument("-c", "--chunk_size", type=int, default=64,
-                        help='chunk size [KB] for network download (default: 64)')
-    parser.add_argument("-l", "--large_file_limit", type=int, default=256,
-                        help='file size [KB] which is downloaded without splitting into chunks (default: 256)')
+    parser.add_argument(
+        "-r", "--record", nargs="+", action="extend", default=[], help="record ID(s)"
+    )
+    parser.add_argument(
+        "-s",
+        "--sandbox",
+        nargs="+",
+        action="extend",
+        default=[],
+        help="sandbox record ID(s)",
+    )
+    parser.add_argument(
+        "-c",
+        "--chunk_size",
+        type=int,
+        default=64,
+        help="chunk size [KB] for network download (default: 64)",
+    )
+    parser.add_argument(
+        "-l",
+        "--large_file_limit",
+        type=int,
+        default=256,
+        help="file size [KB] which is downloaded without splitting into chunks (default: 256)",
+    )
 
-    parser.add_argument("-L", "--log_level",
-                        default='error',
-                        const='error',
-                        nargs='?',
-                        choices=['critical', 'error', 'warning', 'info', 'debug'],
-                        help='log level (default: error)')
+    parser.add_argument(
+        "-L",
+        "--log_level",
+        default="error",
+        const="error",
+        nargs="?",
+        choices=["critical", "error", "warning", "info", "debug"],
+        help="log level (default: error)",
+    )
 
     parser.add_argument("-f", "--foreground", action="store_true", default=False)
     parser.add_argument("-d", "--debug", action="store_true", default=False)
 
     args = parser.parse_args()
 
-    level = {'critical': logging.CRITICAL,
-             'error':    logging.ERROR,
-             'warning':  logging.WARNING,
-             'info':     logging.INFO,
-             'debug':    logging.DEBUG,
-             }[args.log_level]
+    level = {
+        "critical": logging.CRITICAL,
+        "error": logging.ERROR,
+        "warning": logging.WARNING,
+        "info": logging.INFO,
+        "debug": logging.DEBUG,
+    }[args.log_level]
 
     logging.basicConfig(level=level)
 
-    fuse = FUSE(ZenodoFS(args.record, args.sandbox, chunksize=args.chunk_size, largefile=args.large_file_limit),
-                args.mountpoint,
-                foreground=args.foreground,
-                nothreads=True,
-                debug=args.debug
-                )
+    fuse = FUSE(
+        ZenodoFS(
+            args.record,
+            args.sandbox,
+            chunksize=args.chunk_size,
+            largefile=args.large_file_limit,
+        ),
+        args.mountpoint,
+        foreground=args.foreground,
+        nothreads=True,
+        debug=args.debug,
+    )
